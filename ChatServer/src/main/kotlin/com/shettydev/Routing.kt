924322c89a7c85d55a3977bb03b66c681dc1db91
Yttehs <shetty.yttehs@outlook.com>
2024-12-23 15:05:00 +0800

    chore: rewrite main page by html dsl

--- ChatServer/src/main/kotlin/com/shettydev/Routing.kt
+++ ChatServer/src/main/kotlin/com/shettydev/Routing.kt
@@ -1,13 +1,28 @@
 package com.shettydev
 
 import io.ktor.server.application.*
-import io.ktor.server.response.*
+import io.ktor.server.html.*
 import io.ktor.server.routing.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Server is running")
+            call.respondHtml {
+                head {
+                    title { +"Chat Server" }
+                }
+                body {
+                    h1 { +"Chat Server" }
+                    p {
+                        +"Powered by "
+                        a {
+                            href = "https://ktor.io"
+                            +"Ktor"
+                        }
+                    }
+                }
+            }
         }
     }
 }
